0d6071938ad36dcd421c98507819dc6f5af5fe35
drewno026 <wiktor.ppniak@gmail.com>
2024-10-25 19:05:56 +0200

    zadanieDomowe2

--- Lab_02/ZadanieDomowe_02.py
+++ Lab_02/ZadanieDomowe_02.py
@@ -25,13 +25,13 @@ def calculate_cone(radius, height, density):
     volume = math.pi*radius**2*height/3
     mass = density*volume
     return surface_area, volume, mass
-#wszystko
+
 def calculate_cylinder(radius, height, density):
     surface_area = 2*math.pi*radius*(radius+height)
     volume = math.pi*radius**2*height
     mass = density*volume
     return surface_area, volume, mass
-#2 ostatnie
+
 def calculate_ellipse(semi_major_axis, semi_minor_axis, density):
     epsilon = math.sqrt(1 - (semi_minor_axis**2/semi_major_axis**2))
     if epsilon != 0:
@@ -50,69 +50,105 @@ cylinder\nellipse\nWhat you want to calculate: ").lower()
         case "sphere":
             while True:
                 radius = float(input("Insert radius[in meters]: "))
-                density = float(input("Insert density[kg/m^3]: "))
-                if radius > 0 and density > 0:
+                if radius > 0:
+                    if user_choice2 == "mass":
+                        density = float(input("Insert density[kg/m^3]: "))
+                        if density <= 0:
+                            print("Density must be greater than 0")
+                            continue
+                    else:
+                        density = 1
                     surface_area, volume, mass = calculate_sphere(radius, density)
                     break
                 else:
-                    print("All parameters must be grater than 0")
+                    print("All parameters must be greater than 0")
         case "regular tetrahedron":
             while True:
                 edge_length = float(input("Insert edge length[in meters]: "))
-                density = float(input("Insert density[kg/m^3]: "))
-                if edge_length > 0 and density > 0:
+                if edge_length > 0:
+                    if user_choice2 == "mass":
+                        density = float(input("Insert density[kg/m^3]: "))
+                        if density <= 0:
+                            print("Density must be greater than 0")
+                            continue
+                    else:
+                        density = 1
                     surface_area, volume, mass = calculate_regular_tetrahedron(edge_length, density)
                     break
                 else:
-                    print("All parameters must be grater than 0")
+                    print("All parameters must be greater than 0")
         case "rectangular pyramid":
             while True:
                 length = float(input("Insert length[in meters]: "))
                 width = float(input("Insert width[in meters]: "))
                 height = float(input("Insert height[in meters]: "))
-                density = float(input("Insert density[kg/m^3]: "))
-                if length > 0 and width > 0 and height > 0 and density > 0:
+                if length > 0 and width > 0 and height > 0:
+                    if user_choice2 == "mass":
+                        density = float(input("Insert density[kg/m^3]: "))
+                        if density <= 0:
+                            print("Density must be greater than 0")
+                            continue
+                    else:
+                        density = 1
                     surface_area, volume, mass = calculate_rectangular_pyramid(length, width, height, density)
                     break
                 else:
-                    print("All parameters must be grater than 0")
+                    print("All parameters must be greater than 0")
         case "cone":
             while True:
                 radius = float(input("Insert radius[in meters]: "))
                 height = float(input("Insert height[in meters]: "))
-                density = float(input("Insert density[kg/m^3]: "))
-                if radius > 0 and height > 0 and density > 0:
+                if radius > 0 and height > 0:
+                    if user_choice2 == "mass":
+                        density = float(input("Insert density[kg/m^3]: "))
+                        if density <= 0:
+                            print("Density must be greater than 0")
+                            continue
+                    else:
+                        density = 1
                     surface_area, volume, mass = calculate_cone(radius, height, density)
                     break
                 else:
-                    print("All parameters must be grater than 0")
+                    print("All parameters must be greater than 0")
         case "cylinder":
             while True:
                 radius = float(input("Insert radius[in meters]: "))
                 height = float(input("Insert height[in meters]: "))
-                density = float(input("Insert density[kg/m^3]: "))
-                if radius > 0 and height > 0 and density > 0:
+                if radius > 0 and height > 0:
+                    if user_choice2 == "mass":
+                        density = float(input("Insert density[kg/m^3]: "))
+                        if density <= 0:
+                            print("Density must be greater than 0")
+                            continue
+                    else:
+                        density = 1
                     surface_area, volume, mass = calculate_cylinder(radius, height, density)
                     break
                 else:
-                    print("All parameters must be grater than 0")
+                    print("All parameters must be greater than 0")
         case "ellipse":
             while True:
-                radius = float(input("Insert radius[in meters]: "))
-                height = float(input("Insert height[in meters]: "))
-                density = float(input("Insert density[kg/m^3]: "))
-                if radius > 0 and height > 0 and density > 0:
-                    surface_area, volume, mass = calculate_ellipse(radius, height, density)
+                semi_major_axis = float(input("Insert semi-major axis[in meters]: "))
+                semi_minor_axis = float(input("Insert semi-minor axis[in meters]: "))
+                if semi_major_axis > 0 and semi_minor_axis > 0:
+                    if user_choice2 == "mass":
+                        density = float(input("Insert density[kg/m^3]: "))
+                        if density <= 0:
+                            print("Density must be greater than 0")
+                            continue
+                    else:
+                        density = 1
+                    surface_area, volume, mass = calculate_ellipse(semi_major_axis, semi_minor_axis, density)
                     break
                 else:
-                    print("All parameters must be grater than 0")
+                    print("All parameters must be greater than 0")
     match user_choice2:
         case "surface area":
             print(f"Surface area equals: {surface_area:.2f} m^2")
         case "volume":
             print(f"Volume equals: {volume:.2f} m^3")
         case "mass":
-            print(f"Mass equals: {mass:.2f} kg/m^3")
+            print(f"Mass equals: {mass:.2f} kg")
     continue_choice = input("Do you want to continue(yes/no):").lower()
     if continue_choice != "yes":
         break
